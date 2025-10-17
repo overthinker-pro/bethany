dec5b9968bb6d9d61756d03ed70d152215c87025
overthinker-pro <noelvabraham@gmail.com>
2025-10-17 22:30:05 +0530

    comment cleanups

--- js/main.js
+++ js/main.js
@@ -1,20 +1,14 @@
-/* ===================================================================
- * Hesed 1.0.0 - Main JS
- *
- * ------------------------------------------------------------------- */
 
 (function($) {
 
     "use strict";
     
     const cfg = {
-                scrollDuration : 800, // smoothscroll duration
-                mailChimpURL   : ''   // mailchimp url
+                scrollDuration : 600,
+                mailChimpURL   : '' 
                 };
     const $WIN = $(window);
 
-    // Add the User Agent to the <html>
-    // will be used for IE10/IE11 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0; rv:11.0))
     const doc = document.documentElement;
     doc.setAttribute('data-useragent', navigator.userAgent);
 
@@ -27,12 +21,7 @@
 
         $WIN.on('load', function() {
 
-            // force page scroll position to top at page refresh
-            // $('html, body').animate({ scrollTop: 0 }, 'normal');
-
-            // will first fade out the loading animation 
             $("#loader").fadeOut("slow", function() {
-                // will fade out the whole DIV that covers the website.
                 $("#preloader").delay(300).fadeOut("slow");
             }); 
             
